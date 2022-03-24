164cc878543cfec9adb41c15f61743a40d87f22a
kakon4366 <kakonbarman4366@gmail.com>
2022-03-25 01:23:14 +0600

    Cart summary calculation working success

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,6 +3,14 @@ import CartProduct from '../CartProduct/CartProduct';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    
+    console.log(cart);
+    let totalPrice = 0;
+    for(const product of cart){
+        totalPrice += product.price;
+    }
+    const tax = totalPrice * 0.1;
+    const grandTotal = totalPrice + tax;
 
     return (
         <div className='cart-container'>
@@ -14,11 +22,11 @@ const Cart = ({cart}) => {
             <div className='cart-calculation-container'>
                 <h5 className='text-center mt-2 mb-1'>Summary</h5>
                 <hr className='mt-0' />
-                <p>Total Products: </p>
-                <p>Total Price: </p>
-                <p>Tax: </p>
+                <p>Total Products: {cart.length}</p>
+                <p>Total Price: {totalPrice}</p>
+                <p>Tax: {tax.toFixed(2)}</p>
                 <hr />
-                <h6>Grand Total: </h6>
+                <h6>Grand Total: {grandTotal.toFixed(2)}</h6>
 
 
             </div>
