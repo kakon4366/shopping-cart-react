8b62b9990ba9e3e96db88cf9ff5d87eeccd57718
kakon4366 <kakonbarman4366@gmail.com>
2022-03-24 12:02:29 +0600

    Product cart button added and cart style

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './Product.css';
 
 const Product = ({product}) => {
+    console.log(product);
     const {title, image, price, rating, description, category} = product;
     return (
         <div className='product'>
             <div className='product-image'>
                 <img src={image} alt="" />
             </div>
-            <h2>{title}</h2>
+            <h3 title={title}>{title}</h3>
+            <h4>Price: {price}</h4>
+            <p className='product-description'>{description}</p>
+            <p className='rating'>Rating: {rating.rate} ({rating.count})</p>
+            <button className='add-to-cart-button'>Add To Cart</button>
         </div>
     );
 };
